cd55f6ac4c4010ad6f5dd43ecb9d2177db95b99f
Fatih Tuluk <fatih4159@gmail.com>
2023-06-16 19:30:14 +0200

    init

--- app/src/main/java/de/agx/blazingtask/ui/tasks/TasksFragment.java
+++ app/src/main/java/de/agx/blazingtask/ui/tasks/TasksFragment.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TimePicker;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
@@ -22,6 +25,7 @@ import de.agx.blazingtask.ui.types.TaskTime;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class TasksFragment extends Fragment {
 
@@ -48,39 +52,73 @@ public class TasksFragment extends Fragment {
         binding.floatingActionButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Inflate the dialog layout
-                View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.dialog_add_task_time, null);
+                // Create a LinearLayout to hold all the views
+                LinearLayout linearLayout = new LinearLayout(getContext());
+                linearLayout.setOrientation(LinearLayout.VERTICAL);
+                linearLayout.setPadding(50, 20, 50, 20);
 
-                // Initialize all the required views
-                EditText taskDateET = new EditText(getContext());
+                // Initialize all the required views and add them to the LinearLayout
+                DatePicker taskDatePicker = new DatePicker(getContext());
+                linearLayout.addView(taskDatePicker);
+
                 EditText taskUserET = new EditText(getContext());
+                taskUserET.setHint("Task User");
+                linearLayout.addView(taskUserET);
+
                 EditText taskTypeET = new EditText(getContext());
+                taskTypeET.setHint("Task Type");
+                linearLayout.addView(taskTypeET);
+
                 EditText taskNameET = new EditText(getContext());
-                EditText taskTimeStampStartET = new EditText(getContext());;
-                EditText taskTimeStampFinishET = new EditText(getContext());;
-                EditText taskTimeDurationET = new EditText(getContext());;
+                taskNameET.setHint("Task Name");
+                linearLayout.addView(taskNameET);
+
+                TimePicker taskTimeStartPicker = new TimePicker(getContext());
+                taskTimeStartPicker.setIs24HourView(true);
+                linearLayout.addView(taskTimeStartPicker);
+
+                TimePicker taskTimeFinishPicker = new TimePicker(getContext());
+                taskTimeFinishPicker.setIs24HourView(true);
+                linearLayout.addView(taskTimeFinishPicker);
+
+                EditText taskTimeDurationET = new EditText(getContext());
+                taskTimeDurationET.setHint("Task Time Duration");
+                linearLayout.addView(taskTimeDurationET);
 
                 // Create & show the dialog
                 AlertDialog dialog = new AlertDialog.Builder(getContext())
                         .setTitle("Add New Task")
-                        .setView(dialogView)
+                        .setView(linearLayout)
                         .setPositiveButton("Add", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 // Collect data from the views and create a new TaskTime instance
+                                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                                String taskDate = dateFormat.format(new Date(taskDatePicker.getYear() - 1900, taskDatePicker.getMonth(), taskDatePicker.getDayOfMonth()));
+
+                                EditText taskTimeStampStartET = new EditText(getContext());
+                                taskTimeStampStartET.setHint("Task TimeStamp Start");
+                                String taskTimeStampStart = taskTimeStampStartET.getText().toString();
+
+                                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                                    int hour = taskTimeStartPicker.getHour();
+                                    int minute = taskTimeStartPicker.getMinute();
+                                    taskTimeStampStart = String.format(Locale.US, "%02d:%02d:00", hour, minute);
+                                }
+
                                 TaskTime newTaskTime = new TaskTime(
-                                        taskDateET.getText().toString(),
+                                        taskDate,
                                         taskUserET.getText().toString(),
                                         taskTypeET.getText().toString(),
                                         taskNameET.getText().toString(),
-                                        taskTimeStampStartET.getText().toString(),
-                                        taskTimeStampFinishET.getText().toString(),
+                                        taskTimeStampStart,
+                                        "00:00:00",
                                         taskTimeDurationET.getText().toString()
                                 );
 
                                 // Pass the new TaskTime object to your database layer to insert it into the database
                                 AppDatabase db = AppDatabase.getInstance(getContext());
-                                new TaskRepository(db).insert(newTaskTime);
+                                new TaskRepository(db).insertTaskTime(newTaskTime);
                             }
                         })
                         .setNegativeButton("Cancel", null)
